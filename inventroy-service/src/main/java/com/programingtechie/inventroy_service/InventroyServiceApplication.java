e265a64c085392627332181652161fcbfbffc66b
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2025-07-15 13:58:30 +0530

    update inventroy service

---
+++ inventroy-service/src/main/java/com/programingtechie/inventroy_service/InventroyServiceApplication.java
@@ -0,0 +1,14 @@
+package com.programingtechie.inventroy_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventroyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventroyServiceApplication.class, args);
+		System.out.println("Inventory Service Start .......");
+	}
+
+}
--- order-service/src/main/java/com/programingtechie/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/programingtechie/order_service/controller/OrderController.java
@@ -2,16 +2,23 @@ package com.programingtechie.order_service.controller;
 
 
 import com.programingtechie.order_service.dto.OrderRequest;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.programingtechie.order_service.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/orders")
 public class OrderController {
 
+    private final OrderService orderService;
 
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public String createOrders(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
         return "Order Successfully Added !";
     }
 }
